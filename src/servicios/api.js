c21444117e78d54a643331cba1c6e6fa0ef4a132
Daniela Gonzalez <vazquezdanig64@gmail.com>
2026-04-21 15:42:54 -0600

    Front-end

--- src/servicios/api.js
+++ src/servicios/api.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
-// Si existe la variable de entorno la usa, si no, usa la de Render directamente
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
     "https://backendbaguette-1.onrender.com/api/v1",
+});
+
+// Este interceptor añade la API KEY a todas las peticiones automáticamente
+API.interceptors.request.use((config) => {
+  config.headers["x-api-key"] = "full-stack"; // Debe ser igual a la variable API_KEY en Render
+  return config;
 });
 
 export default API;
